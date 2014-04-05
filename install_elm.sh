f6b70e245e4bd5097897850338b6b4b6b7d287d6
Simon Buist <simon.buist@gmail.com>
2014-04-05 01:01:04 +0100

    Update

--- install_elm.sh
+++ install_elm.sh
@@ -1,23 +1,22 @@
-1. Install ghc 7.6.3
+#!/bin/bash
 
-    1. `sudo apt-get install libgl1-mesa-dev libglc-dev freeglut3-dev
-       libedit-dev libglw1-mesa libglw1-mesa-dev`
-    2. `sudo apt-get install ghc`
-    3. `wget http://www.haskell.org/ghc/dist/7.6.3/ghc-7.6.3-src.tar.bz2`
-    4. `tar xjf ghc-7.6.3-src.tar.bz2`
-    5. `cd ghc-7.6.3`
-    6. `./configure`
-    7. `make -j 8`
-    8. `sudo make install`
-    9. `wget`
-    1. `http://lambda.haskell.org/platform/download/2013.2.0.0/haskell-platform-2013.2.0.0.tar.gz`
-    1. `tar xzvf haskell-platform-2013.2.0.0.tar.gz`
-    1. `cd haskell-platform-2013.2.0.0`
-    1. `./configure`
-    1. `make`
-    1. `sudo make install`
-
-2. `cabal update`
-3. `cabal install cabal-install`
-4. `cabal install elm`
-5. `cabal install elm-server`
+exec sudo apt-get install libgl1-mesa-dev libglc-dev freeglut3-dev libedit-dev libglw1-mesa libglw1-mesa-dev
+exec sudo apt-get install ghc
+exec wget http://www.haskell.org/ghc/dist/7.6.3/ghc-7.6.3-src.tar.bz2
+exec tar xjf ghc-7.6.3-src.tar.bz2
+exec cd ghc-7.6.3
+exec ./configure
+exec make -j 8
+exec sudo make install
+exec wget
+exec http://lambda.haskell.org/platform/download/2013.2.0.0/haskell-platform-2013.2.0.0.tar.gz
+exec tar xzvf haskell-platform-2013.2.0.0.tar.gz
+exec cd haskell-platform-2013.2.0.0
+exec ./configure
+exec make
+exec sudo make install
+exec cabal update
+exec cabal install cabal-install
+exec cabal install elm
+exec cabal install elm-server
+exit 0
