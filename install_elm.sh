7dd73b0ed66ebb08cff2425ee639dc06dcd81cb0
Simon Buist <simon.buist@gmail.com>
2014-04-05 01:15:35 +0100

    Suppress output of tar

--- install_elm.sh
+++ install_elm.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-exec sudo apt-get install libgl1-mesa-dev libglc-dev freeglut3-dev libedit-dev libglw1-mesa libglw1-mesa-dev
-exec sudo apt-get install ghc
-exec wget http://www.haskell.org/ghc/dist/7.6.3/ghc-7.6.3-src.tar.bz2
-exec tar xjf ghc-7.6.3-src.tar.bz2
-exec cd ghc-7.6.3
-exec ./configure
-exec make -j 8
-exec sudo make install
-exec wget http://lambda.haskell.org/platform/download/2013.2.0.0/haskell-platform-2013.2.0.0.tar.gz
-exec tar xzvf haskell-platform-2013.2.0.0.tar.gz
-exec cd haskell-platform-2013.2.0.0
-exec ./configure
-exec make
-exec sudo make install
-exec cabal update
-exec cabal install cabal-install
-exec cabal install elm
-exec cabal install elm-server
+sudo apt-get install libgl1-mesa-dev libglc-dev freeglut3-dev libedit-dev libglw1-mesa libglw1-mesa-dev
+sudo apt-get install ghc
+wget http://www.haskell.org/ghc/dist/7.6.3/ghc-7.6.3-src.tar.bz2
+tar xjf ghc-7.6.3-src.tar.bz2
+cd ghc-7.6.3
+./configure
+make -j 8
+sudo make install
+wget http://lambda.haskell.org/platform/download/2013.2.0.0/haskell-platform-2013.2.0.0.tar.gz
+tar xzf haskell-platform-2013.2.0.0.tar.gz
+cd haskell-platform-2013.2.0.0
+./configure
+make
+sudo make install
+cabal update
+cabal install cabal-install
+cabal install elm
+cabal install elm-server
 exit 0
