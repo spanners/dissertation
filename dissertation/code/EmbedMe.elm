599272af6b393a80598d14c75aa6750f7e527c0e
Simon Buist <simon.buist@gmail.com>
2014-04-23 00:50:35 +0100

    Update code

--- dissertation/code/EmbedMe.elm
+++ dissertation/code/EmbedMe.elm
@@ -13,10 +13,6 @@ import Window
 (~>) = flip lift
 infixl 4 ~>
 
-(~~>) = flip lift2
-
-(<~~) = (<|)
-
 type Stamp = { t: Float, x: Float, y: Float }
 
 -- Incoming
@@ -27,23 +23,32 @@ stamps : Signal [Stamp]
 stamps = foldp (::) [] stamped
 
 scene (w,h) locs =
-  let drawPentagon {t,x,y} =
-          ngon 5 20 |> filled (hsva y 1 1 0.7)
-                    |> move (x - toFloat w / 2, toFloat h / 2 - y)
-                    |> rotate x
-  in  collage w h (map drawPentagon locs)
+  let drawCircle {t, x, y} =
+          circle 5 |> filled (hsva y 1 1 0.7)
+                   |> move (x - toFloat w / 2, toFloat h / 2 - y)
+  in  collage w h (map drawCircle locs)
 
 main = lift2 scene Window.dimensions stamps
 
 -- Outgoing
 
+-- Do not change user_id = "1" 
+-- It gets replaced with the actual user_id when compiled to JS
 user_id = "1"
 
-firebaseRequest requestType requestData = Http.request requestType ("https://sweltering-fire-9141.firebaseio.com/dissertation/" ++ user_id ++ ".json") requestData []
+firebaseRequest requestType requestData = 
+  Http.request requestType 
+               ("https://sweltering-fire-9141.firebaseio.com/"
+	          ++ "dissertation/" 
+		  ++ user_id 
+                  ++ ".json")
+               requestData 
+               []
 
-
-
-serialize r = r |> JEXP.fromRecord |> Json.fromJSObject |> Json.toJSString " " |> JS.toString
+serialize r = r |> JEXP.fromRecord 
+                |> Json.fromJSObject 
+		|> Json.toJSString " " 
+		|> JS.toString
 
 toRequestData (t, (x, y)) = {t = t, x = x, y = y } |> serialize
 
@@ -51,7 +56,7 @@ clicks = timestamp (sampleOn Mouse.isDown Mouse.position)
 
 toRequest click = case click of
   (0, (0, 0)) -> firebaseRequest "get" ""
-  (t, (x, y)) -> firebaseRequest "post" (click |> toRequestData)
+  _           -> firebaseRequest "post" (click |> toRequestData)
 
 requests = clicks ~> toRequest
 
