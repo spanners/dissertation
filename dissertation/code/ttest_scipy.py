ff31b6db03b05654f018593e84983e61191eff13
Simon Buist <simon.buist@gmail.com>
2014-04-25 01:27:13 +0100

    Update code to add chi-squared calculation

--- dissertation/code/ttest_scipy.py
+++ dissertation/code/ttest_scipy.py
@@ -7,3 +7,11 @@ js = [33,12,69,3]
 outcome = stats.ttest_ind(elm,js, equal_var=False)
 
 print("t-value: %4.2f, p-value: %4.2f" % (outcome[0], outcome[1]))
+
+observed = elm + js
+
+expected = sum(observed)/(len(observed)*1.0)
+
+chi_squared = sum([((x - expected)**2)/expected for x in observed])
+
+print("chi-squared : %4.2f" % chi_squared)
